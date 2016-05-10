5441cf974eb5dc99fe0d64a1031a75292495277a
Ream Lim <rlim@humanlongevity.com>
2016-05-10 11:11:25 +0800

    add problem 016

---
+++ python/Problem016.py
@@ -0,0 +1,7 @@
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 2^1000?
+
+num = 1000
+
+print "Solution is %s " % sum([int(x) for x in str(2 ** num)])
