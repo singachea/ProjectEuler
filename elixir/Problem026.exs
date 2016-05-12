6fb635a3e60f319a281a843c0882d7b6e1922a19
Ream Lim <singachea@gmail.com>
2016-05-12 22:50:47 +0800

    add problem 026

--- elixir/Problem026.exs
+++ elixir/Problem026.exs
@@ -1,34 +1,52 @@
+# A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+#
+# 1/2	= 	0.5
+# 1/3	= 	0.(3)
+# 1/4	= 	0.25
+# 1/5	= 	0.2
+# 1/6	= 	0.1(6)
+# 1/7	= 	0.(142857)
+# 1/8	= 	0.125
+# 1/9	= 	0.(1)
+# 1/10	= 	0.1
+# Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+#
+# Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
 defmodule Solution do
+  def dlen(numerator, denominator) do
+    find_pattern(numerator, denominator, [], [])
+      |> Enum.join("")
+      |> String.length
+  end
 
-  def find_pattern(numerator, denominator, dlist, rlist) do
-    # list = [numerator | list]
-    r = rem(numerator * 10, denominator)
-    d = div(numerator * 10, denominator)
-
-    index = Enum.find_index(rlist, &(&1 == r))
-    rlist = [r | rlist]
-    IO.inspect {d, r}
-    cond do
-      r == 0 -> dlist
-      index -> [d | (dlist |> Enum.slice(0, index) |> Enum.reverse)]
-      true -> find_pattern(r, denominator, [d | dlist], rlist)
+  defp expand_numerator(numerator, denominator, leading_zeroes) do
+    case numerator do
+      x when x < denominator -> expand_numerator(numerator * 10, denominator, "0" <> leading_zeroes)
+      _ -> {numerator, leading_zeroes}
     end
   end
 
-  def dlen(numerator, denominator) do
-    length(find_pattern(numerator, denominator, [], []))
-  end
+  defp find_pattern(numerator, denominator, dlist, rlist) do
 
+    {numerator, leading_zeroes} = expand_numerator(numerator * 10, denominator, "") # we need leading_zeroes. e.g. for case 1 / 13 => 10 / 13 => 100 / 13
+
+    r = rem(numerator, denominator) # remainder
+    d = leading_zeroes <> Integer.to_string(div(numerator, denominator))
+
+    index = Enum.find_index(rlist, &(&1 == r)) # find index from the head of array to see the repeat pattern
+
+    cond do
+      r == 0 -> [] # no repeat
+      index -> [d | (dlist |> Enum.slice(index + 1, length(dlist)))] # repeat here, and we attach the last one to head and slice from index + 1
+      true -> find_pattern(r, denominator,  dlist ++ [d], rlist ++ [r]) # I don't use | because i don't want ot reverse and mess up the thought process
+    end
+  end
 end
 
+{ _, max_index } = 1..999
+                    |> Enum.map(&Solution.dlen(1, &1))
+                    |> Enum.with_index(1)
+                    |> Enum.max_by(fn {value, _} -> value end)
 
-Solution.find_pattern(1, 12, [], [])
-
-
-#
-# max = 1..999
-#         |> Enum.map(&Solution.dlen(1, &1))
-#         |> Enum.max
-#
-#
-# IO.puts "Solution is #{max}"
+IO.puts "Solution is #{max_index}"
