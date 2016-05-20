e75d7d03b9ac6d2362e2475df02f8a3e60d280ab
Ream Lim <singachea@gmail.com>
2016-05-20 17:38:55 +0800

    clean up problem 054

--- elixir/Problem054/Problem054.exs
+++ elixir/Problem054/Problem054.exs
@@ -1,7 +1,3 @@
-
-
-
-
 defmodule Solution do
   def card_value(c) do
     case c do
@@ -9,11 +5,12 @@ defmodule Solution do
       "K" -> 13
       "Q" -> 12
       "J" -> 11
+      "T" -> 10
       x -> String.to_integer(x)
     end
   end
 
-  def flush?(cards) do
+  def flush?(cards) do # or can use parttern matching here
     {_, color} = Enum.at(cards, 0)
     Enum.all?(cards, fn {_, c} -> c == color end)
   end
@@ -29,7 +26,7 @@ defmodule Solution do
     array = cards
     |> Enum.map(fn {v, _} -> card_value(v) end)
     |> Enum.group_by(&(&1))
-    |> Enum.map(fn {k, v} -> {k, length(v)} end)
+    |> Enum.map(fn {k, v} -> {k, length(v)} end) # result is in [{key: length}]
 
     cond do
       straight?(cards) and flush?(cards) -> rank_straight_flush(array)
@@ -61,131 +58,74 @@ defmodule Solution do
   defp check_type([1, 4]), do: :four_of_a_kind
 
 
-  def rank_pairs(array) do
-    {pairs, rest} =  array |> Enum.partition(fn {_, v} -> v == 2 end)
+  defp partition_by_size(array, type_count, high_zeroes, low_zeroes ) do
+    {pairs, rest} =  array |> Enum.partition(fn {_, v} -> v == type_count end)
     pairs = pairs
             |> Enum.map(fn {v, _} -> v end)
             |> Enum.sort(fn a, b -> a > b end)
             |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}0000")) # 4 zeroes
+            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}#{String.duplicate("0", high_zeroes)}"))
             |> Enum.join("")
 
     rest = rest
             |> Enum.map(fn {v, _} -> v end)
             |> Enum.sort(fn a, b -> a > b end)
             |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&(String.rjust(&1, 2, ?0)))
+            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}#{String.duplicate("0", low_zeroes)}"))
             |> Enum.join("")
 
     "#{pairs}#{rest}"
+  end
+
+  defp rank_padding(array, zeroes) do
+    r = array
+      |> Enum.map(fn {v, _} -> v end)
+      |> Enum.sort(fn a, b -> a > b end)
+      |> Enum.map(&Integer.to_string(&1))
+      |> Enum.map(&("#{String.rjust(&1, 2, ?0)}"))
+      |> Enum.join("")
+
+
+    "#{r}#{String.duplicate("0", zeroes)}"
+  end
+
+  defp rank_pairs(array) do
+    partition_by_size(array, 2, 4, 0)
     # 1 p -> (1|2)0000(6) = 11|12 digits
     # 2 p -> (1|2)0000(2)0000(2) = 13|14 digits
   end
 
-  def rank_three(array) do
-    {pairs, rest} =  array |> Enum.partition(fn {_, v} -> v == 3 end)
-    pairs = pairs
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}000000000")) # 9 zeroes
-            |> Enum.join("")
-
-    rest = rest
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&(String.rjust(&1, 2, ?0)))
-            |> Enum.join("")
-
-    "#{pairs}#{rest}"
+  defp rank_three(array) do
+    partition_by_size(array, 3, 9, 0)
     # 3 p -> (1|2)000000000(4) = 14|15 digits
   end
 
-
-  def rank_straight(array) do
-    r = array
-      |> Enum.map(fn {v, _} -> v end)
-      |> Enum.sort(fn a, b -> a > b end)
-      |> Enum.map(&Integer.to_string(&1))
-      |> Enum.map(&("#{String.rjust(&1, 2, ?0)}"))
-      |> Enum.join("")
-
-
-    "#{r}000000" # 15|16 digits
+  defp rank_straight(array) do
+    rank_padding(array, 6)
   end
 
-  def rank_flush(array) do
-    r = array
-      |> Enum.map(fn {v, _} -> v end)
-      |> Enum.sort(fn a, b -> a > b end)
-      |> Enum.map(&Integer.to_string(&1))
-      |> Enum.map(&("#{String.rjust(&1, 2, ?0)}"))
-      |> Enum.join("")
-
-
-    "#{r}0000000" # 16|17 digits
+  defp rank_flush(array) do
+    rank_padding(array, 7)
   end
 
-  def rank_full_house(array) do
-    {pairs, rest} =  array |> Enum.partition(fn {_, v} -> v == 3 end)
-    pairs = pairs
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}0000000000")) # 9 zeroes
-            |> Enum.join("")
-
-    rest = rest
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}0000")) # 4 zeroes
-            |> Enum.join("")
-
-    "#{pairs}#{rest}"
+  defp rank_full_house(array) do
+    partition_by_size(array, 3, 9, 4)
     # 3 p, 2p -> (1|2)0000000000(2)0000 = 17|18 digits
   end
 
-  def rank_four(array) do
-    {pairs, rest} =  array |> Enum.partition(fn {_, v} -> v == 4 end)
-    pairs = pairs
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&("#{String.rjust(&1, 2, ?0)}000000000000000")) # 15 zeroes
-            |> Enum.join("")
-
-    rest = rest
-            |> Enum.map(fn {v, _} -> v end)
-            |> Enum.sort(fn a, b -> a > b end)
-            |> Enum.map(&Integer.to_string(&1))
-            |> Enum.map(&(String.rjust(&1, 2, ?0)))
-            |> Enum.join("")
-
-    "#{pairs}#{rest}"
+  defp rank_four(array) do
+    partition_by_size(array, 5, 15, 0)
     # 4 p -> (1|2)000000000000000(2) = 18|19 digits
   end
 
-
-  def rank_straight_flush(array) do
-    r = array
-      |> Enum.map(fn {v, _} -> v end)
-      |> Enum.sort(fn a, b -> a > b end)
-      |> Enum.map(&Integer.to_string(&1))
-      |> Enum.map(&("#{String.rjust(&1, 2, ?0)}"))
-      |> Enum.join("")
-
-
-    "#{r}0000000000" # 19|20 digits
+  defp rank_straight_flush(array) do
+    rank_padding(array, 10)
   end
 
-
-  def rank_royal_flush(array) do
-    rank_straight_flush(array)
-  end
-
-
+  # we don't need this one.
+  # defp rank_royal_flush(array) do
+  #   rank_straight_flush(array)
+  # end
 end
 
 
@@ -196,7 +136,6 @@ lines = content
                         |> String.split(~r{\s}, trim: true)
                         |> Enum.map(fn el -> el
                                           |> String.split("", trim: true)
-                                          |> Enum.map(fn x -> (String.replace(x, "T", "10") ) end)
                                           |> List.to_tuple end)
                         |> Enum.split(5)))
 
